6af4acdc22b036d3a8e999b4e7760b04c00522dc
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2024-03-05 11:38:46 +0530

    using libraries

---
+++ vars/nodejs.groovy
@@ -0,0 +1,29 @@
+def call() {
+    node('workstation') {
+        stage('Build') {
+            sh 'npm install'
+        }
+        stage('Unit Test') {
+            echo "Unit testing"
+        }
+        stage('Code Analysis') {
+            echo "Code Analysis"
+            // sh 'sudo sonar-scanner -Dsonar.host.url=http://172.31.10.14:9000  -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart -Dsonar.qualitygate.wait=true'
+        }
+        stage('Security Scans') {
+            echo "Security Scans"
+        }
+        if (env.TAG_NAME ==~ ".*") {
+            stage('Publish Artifacts') {
+                echo "Publish Artifacts"
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
